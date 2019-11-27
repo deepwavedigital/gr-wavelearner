24fcc58a5c912e539d3b40175f80800c8ec749ae
John Ferguson <john@deepwavedigital.com>
2019-11-26 19:08:21 -0500

    Replaced the data file with an AIR-T source stream.

--- examples/classifier_test.py
+++ examples/classifier_test.py
@@ -4,8 +4,8 @@
 # GNU Radio Python Flow Graph
 # Title: Classifier Test
 # Author: Deepwave Digital, Inc
-# Description: Test for Wavelearn Inference Block
-# Generated: Tue Nov 26 18:01:32 2019
+# Description: Example for Wavelearner Inference Block
+# Generated: Tue Nov 26 18:57:38 2019
 ##################################################
 
 
@@ -15,6 +15,7 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
+import soapy
 import wavelearner
 
 
@@ -26,30 +27,41 @@ class classifier_test(gr.top_block):
         ##################################################
         # Variables
         ##################################################
-        self.output_len = output_len = 16
-        self.input_len = input_len = 4096
-        self.fs = fs = 100e6
-        self.data_path = data_path = "/usr/local/src/gr-wavelearner/examples/"
-        self.batch_size = batch_size = 4
+        self.samp_rate = samp_rate = 31.25e6
+        self.output_len = output_len = 12
+        self.input_len = input_len = 2048
+        self.frequency = frequency = 2.4e9
+        self.batch_size = batch_size = 128
 
         ##################################################
         # Blocks
         ##################################################
         self.wavelearner_terminal_sink_0 = wavelearner.terminal_sink(output_len*batch_size, batch_size)
-        self.type_confert = blocks.short_to_float(1, 32768)
-        self.throttle = blocks.throttle(gr.sizeof_short*1, fs,True)
-        self.source = blocks.file_source(gr.sizeof_short*1, data_path + "classifier_test.dat", False)
-        self.inference = wavelearner.inference(data_path + "classifier_test_tx2_trt4.plan", False, input_len*batch_size, output_len*batch_size, batch_size)
-        self.buffer0 = blocks.stream_to_vector(gr.sizeof_float*1, batch_size*input_len)
+        self.soapy_source_0 = \
+         soapy.source(1, "device=SoapyAIRT", '', samp_rate, "fc32")
+
+
+
+        self.soapy_source_0.set_frequency(0, frequency)
+        self.soapy_source_0.set_gain(0, 0)
+        self.soapy_source_0.set_gain_mode(0, True)
+        self.soapy_source_0.set_dc_offset_mode(0, True)
+
+        self.inference = wavelearner.inference('saved_model.plan', True, input_len*batch_size, output_len*batch_size, batch_size)
+        self.buffer0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, batch_size*input_len)
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.buffer0, 0), (self.inference, 0))
         self.connect((self.inference, 0), (self.wavelearner_terminal_sink_0, 0))
-        self.connect((self.source, 0), (self.throttle, 0))
-        self.connect((self.throttle, 0), (self.type_confert, 0))
-        self.connect((self.type_confert, 0), (self.buffer0, 0))
+        self.connect((self.soapy_source_0, 0), (self.buffer0, 0))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
 
     def get_output_len(self):
         return self.output_len
@@ -63,19 +75,12 @@ class classifier_test(gr.top_block):
     def set_input_len(self, input_len):
         self.input_len = input_len
 
-    def get_fs(self):
-        return self.fs
+    def get_frequency(self):
+        return self.frequency
 
-    def set_fs(self, fs):
-        self.fs = fs
-        self.throttle.set_sample_rate(self.fs)
-
-    def get_data_path(self):
-        return self.data_path
-
-    def set_data_path(self, data_path):
-        self.data_path = data_path
-        self.source.open(self.data_path + "classifier_test.dat", False)
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+        self.soapy_source_0.set_frequency(0, self.frequency)
 
     def get_batch_size(self):
         return self.batch_size
@@ -90,6 +95,11 @@ def main(top_block_cls=classifier_test, options=None):
 
     tb = top_block_cls()
     tb.start()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
     tb.wait()
 
 
