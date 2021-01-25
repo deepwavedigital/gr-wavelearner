55a5116bd6328e62f511a486f931307d18718f81
Peter Witkowski <pete@deepwavedigital.com>
2021-01-25 11:48:27 -0700

    Updated copyright dates.

--- lib/inference_impl.h
+++ lib/inference_impl.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2018-2019 Deepwave Digital Inc.
+ * Copyright 2018-2021 Deepwave Digital Inc.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
