837705382ab24cd01bb8f8e49f7a7bb7b62d89c6
Peter Witkowski <pete@deepwavedigital.com>
2021-07-12 16:08:21 -0600

    Replace boost::shared_ptr with std::shared_ptr.

--- include/wavelearner/inference.h
+++ include/wavelearner/inference.h
@@ -1,23 +1,9 @@
 /* -*- c++ -*- */
-/* 
- * Copyright 2018-2019 Deepwave Digital Inc.
- * 
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+/*
+ * Copyright 2018-2019, 2021 Deepwave Digital Inc.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
-
 
 #ifndef INCLUDED_WAVELEARNER_INFERENCE_H
 #define INCLUDED_WAVELEARNER_INFERENCE_H
@@ -27,29 +13,29 @@
 #include <gnuradio/sync_block.h>
 
 namespace gr {
-  namespace wavelearner {
+namespace wavelearner {
 
-    class WAVELEARNER_API inference : virtual public gr::sync_block {
-     public:
-      typedef boost::shared_ptr<inference> sptr;
+class WAVELEARNER_API inference : virtual public gr::sync_block {
+ public:
+  typedef std::shared_ptr<inference> sptr;
 
-      /*!
-       * \brief Return a shared_ptr to a new instance of wavelearner::inference.
-       *
-       * To avoid accidental use of raw pointers, wavelearner::inference's
-       * constructor is in a private implementation
-       * class. wavelearner::inference::make is the public interface for
-       * creating new instances.
-       */
-      static sptr make(const std::string& plan_filepath,
-                       const bool complex_input,
-                       const size_t input_vlen,
-                       const size_t output_vlen,
-                       const size_t batch_size);
-    };
+  /*!
+   * \brief Return a shared_ptr to a new instance of wavelearner::inference.
+   *
+   * To avoid accidental use of raw pointers, wavelearner::inference's
+   * constructor is in a private implementation
+   * class. wavelearner::inference::make is the public interface for
+   * creating new instances.
+   */
+  static sptr make(const std::string& plan_filepath,
+                   const bool complex_input,
+                   const size_t input_vlen,
+                   const size_t output_vlen,
+                   const size_t batch_size);
+};
 
-  } // namespace wavelearner
+} // namespace wavelearner
 } // namespace gr
 
-#endif /* INCLUDED_WAVELEARNER_INFERENCE_H */
+#endif  // INCLUDED_WAVELEARNER_INFERENCE_H
 
